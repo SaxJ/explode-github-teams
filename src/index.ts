125f081635942a34bad9033576517b9fc6f3a028
Hen <hello@hen.zone>
2020-01-14 10:41:34 +1100

    master: log all async or sync errors

--- src/index.ts
+++ src/index.ts
@@ -2,25 +2,30 @@ import { Application } from 'probot' // eslint-disable-line no-unused-vars
 
 export = (app: Application) => {
   app.on('pull_request.review_requested', async (context) => {
-    context.payload.pull_request.requested_teams.forEach((team) => {
-      context.log.info(team.id)
-      const teamId = team.id
-      const membersResp = context.github.teams.listMembers({
-        team_id: teamId
-      })
-      membersResp.then((response) => {
-        const members = response.data.map((m) => m.login)
-          .filter((item) => item !== context.payload.pull_request.user.login)
-          .sort((a, b) => 0.5 - Math.random())
+    const handleError = (reason: unknown) => {
+      context.log.error('Failure!', reason)
+    }
 
-        context.github.pulls.createReviewRequest({
-          reviewers: members,
-          pull_number: context.payload.pull_request.number,
-          ...context.repo()
+    try {
+      context.payload.pull_request.requested_teams.forEach((team) => {
+        context.log.info(team.id)
+        const teamId = team.id
+        const membersResp = context.github.teams.listMembers({
+          team_id: teamId
         })
-      }, (value) => {
-        context.log.error('Failure!', value)
+
+        return membersResp.then((response) => {
+          const members = response.data.map((m) => m.login)
+            .filter((item) => item !== context.payload.pull_request.user.login)
+            .sort((a, b) => 0.5 - Math.random())
+
+          return context.github.pulls.createReviewRequest({
+            reviewers: members,
+            pull_number: context.payload.pull_request.number,
+            ...context.repo()
+          })
+        }).catch(handleError)
       })
-    })
+    } catch (error) { handleError(error) }
   })
 }
