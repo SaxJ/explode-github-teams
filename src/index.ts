9dc1dcbeeef392bcbfb3bc4874c680d67fb29ee9
Saxon Jensen <saxon.jensen@healthengine.com.au>
2020-01-13 15:56:31 +0800

    Initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,23 @@
 import { Application } from 'probot' // eslint-disable-line no-unused-vars
 
 export = (app: Application) => {
-  app.on('issues.opened', async (context) => {
-    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
-    await context.github.issues.createComment(issueComment)
+  app.on('pull_request.review_requested', async (context) => {
+    context.payload.pull_request.requested_teams.forEach((team) => {
+      context.log.info(team.id)
+      const teamId = team.id
+      const membersResp = context.github.teams.listMembers({
+        team_id: teamId
+      })
+      membersResp.then((response) => {
+        const members = response.data.map((m) => m.login).filter((item) => item !== context.payload.pull_request.user.login)
+        context.github.pulls.createReviewRequest({
+          reviewers: members,
+          pull_number: context.payload.pull_request.number,
+          ...context.repo()
+        })
+      }, (value) => {
+        context.log.error('Failure!', value)
+      })
+    })
   })
-  // For more information on building apps:
-  // https://probot.github.io/docs/
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
 }
