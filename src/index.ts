bd9d54cd92026c54366b8f5c629c21b857d28a8b
Hen <hello@hen.zone>
2020-01-14 10:58:58 +1100

    flatten handler with await syntax

--- src/index.ts
+++ src/index.ts
@@ -2,35 +2,29 @@ import { Application } from 'probot' // eslint-disable-line no-unused-vars
 
 export = (app: Application) => {
   app.on('pull_request.review_requested', async context => {
-    const handleError = (reason: unknown) => {
-      context.log.error('Failure!', reason)
-    }
-
     try {
-      context.payload.pull_request.requested_teams.forEach(team => {
-        context.log.info(team.id)
-        const teamId = team.id
-        const membersResp = context.github.teams.listMembers({
-          team_id: teamId
-        })
-
-        return membersResp
-          .then(response => {
-            const members = response.data
-              .map(m => m.login)
-              .filter(item => item !== context.payload.pull_request.user.login)
-              .sort((a, b) => 0.5 - Math.random())
-
-            return context.github.pulls.createReviewRequest({
-              reviewers: members,
-              pull_number: context.payload.pull_request.number,
-              ...context.repo()
-            })
+      await Promise.all(
+        context.payload.pull_request.requested_teams.map(async team => {
+          context.log.info(team.id)
+          const teamId = team.id
+          const response = await context.github.teams.listMembers({
+            team_id: teamId
           })
-          .catch(handleError)
-      })
+
+          const members = response.data
+            .map(m => m.login)
+            .filter(item => item !== context.payload.pull_request.user.login)
+            .sort((a, b) => 0.5 - Math.random())
+
+          await context.github.pulls.createReviewRequest({
+            reviewers: members,
+            pull_number: context.payload.pull_request.number,
+            ...context.repo()
+          })
+        })
+      )
     } catch (error) {
-      handleError(error)
+      context.log.error('Failure!', error)
     }
   })
 };
