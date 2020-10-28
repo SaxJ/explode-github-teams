bf4a01491fef2cc93046e7387e0db344b253aa5a
Hen <hello@hen.zone>
2020-10-28 18:06:38 +1100

    fix-race-condition: Remove teams before adding members

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,49 @@
 import { Application } from 'probot' // eslint-disable-line no-unused-vars
 
 export = (app: Application) => {
-  app.on('pull_request.review_requested', async context => {
+  app.on('pull_request.review_requested', async (context) => {
+    const { payload, github, repo, log } = context
+    const pr = payload.pull_request
+
     try {
-      await Promise.all(
-        context.payload.pull_request.requested_teams.map(async team => {
-          context.log.info(team.id)
-          const teamId = team.id
-          const response = await context.github.teams.listMembers({
-            team_id: teamId
-          })
+      const teamIds: number[] = pr.requested_teams.map((team) => team.id)
 
-          const members = response.data
-            .map(m => m.login)
-            .filter(item => item !== context.payload.pull_request.user.login)
-            .sort((a, b) => 0.5 - Math.random())
+      /** If no teams have been added, there is nothing for us to do */
+      if (teamIds.length === 0) return
 
-          await context.github.pulls.createReviewRequest({
-            reviewers: members,
-            pull_number: context.payload.pull_request.number,
-            ...context.repo()
-          })
-        })
+      /** Member responses for each team */
+      const teamLists = await Promise.all(
+        teamIds.map(async (id) => github.teams.listMembers({ team_id: id }))
       )
+
+      /** a flat, randomized list of member logins to add, excluding the owner */
+      const membersToAdd = uniqueValues(
+        teamLists
+          .map((response) => response.data)
+          .flat()
+          .map((member) => member.login)
+          .filter((login) => login !== pr.user.login)
+      ).sort(randomize)
+
+      /** Remove the teams */
+      await github.pulls.deleteReviewRequest({
+        team_reviewers: pr.requested_teams.map((team) => team.slug),
+        pull_number: pr.number,
+        ...repo()
+      })
+
+      /** Add the members explicitly */
+      await github.pulls.createReviewRequest({
+        reviewers: membersToAdd,
+        pull_number: pr.number,
+        ...repo()
+      })
     } catch (error) {
-      context.log.error('Failure!', error)
+      log.error('Failure!', error)
     }
   })
 };
+
+const randomize = () => 0.5 - Math.random()
+
+const uniqueValues = <T>(list: T[]) => [...new Set(list)]
